2eb4712e8db991ceb6fd3c347b21c46dbafc74d7
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-10-25 15:42:28 -0300

    Creo el punto de entrada, que es por donde se ingresara el tamaño de la matriz de coeficientes

---
+++ appDesktop/TPMatematicaSuperiorSIEL/PuntoDeEntrada.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TPMatematicaSuperiorSIEL
+{
+    partial class PuntoDeEntrada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCant = new System.Windows.Forms.TextBox();
+            this.btnContinuar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCant
+            // 
+            this.txtCant.Location = new System.Drawing.Point(79, 112);
+            this.txtCant.Name = "txtCant";
+            this.txtCant.Size = new System.Drawing.Size(100, 20);
+            this.txtCant.TabIndex = 0;
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.Location = new System.Drawing.Point(92, 138);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(75, 23);
+            this.btnContinuar.TabIndex = 1;
+            this.btnContinuar.Text = "Ingresar";
+            this.btnContinuar.UseVisualStyleBackColor = true;
+            this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cantidad de ecuaciones";
+            // 
+            // PuntoDeEntrada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnContinuar);
+            this.Controls.Add(this.txtCant);
+            this.Name = "PuntoDeEntrada";
+            this.Text = "PuntoDeEntrada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCant;
+        private System.Windows.Forms.Button btnContinuar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ appDesktop/TPMatematicaSuperiorSIEL/PuntoDeEntrada.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPMatematicaSuperiorSIEL
+{
+    public partial class PuntoDeEntrada : Form
+    {
+        public PuntoDeEntrada()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int cantidadEcuaciones = int.Parse( txtCant.Text);
+                InterfazGrafica formRoles = new InterfazGrafica(cantidadEcuaciones);
+                this.Hide();
+                formRoles.Show();
+            }catch(Exception ex){
+                MessageBox.Show("Debe ingresar un numero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+       
+    }
+}
