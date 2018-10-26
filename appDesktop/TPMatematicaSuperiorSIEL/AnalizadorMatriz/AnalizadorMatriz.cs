fe80841c667785f0de9c9b752424f5c3f1fba5fe
natoszme <natoszmedra@gmail.com>
2018-10-26 16:09:06 -0300

    Codeo dicha clase

--- appDesktop/TPMatematicaSuperiorSIEL/AnalizadorMatriz/AnalizadorMatriz.cs
+++ appDesktop/TPMatematicaSuperiorSIEL/AnalizadorMatriz/AnalizadorMatriz.cs
@@ -15,9 +15,43 @@ namespace TPMatematicaSuperiorSIEL.AnalizadorMatriz
             NoDominante
         }
 
-        public static DiagonalidadMatriz obtenerDiagonalidad(List<List<double>> coeficientes)
+        public static DiagonalidadMatriz obtenerDiagonalidad(List<List<double>> coeficientes, int tamanioMatriz)
         {
-            return DiagonalidadMatriz.NoDominante;
+            bool esEstrictamenteDominante = true;
+
+            for (int fila = 0; fila < tamanioMatriz ; fila++)
+            {
+                double coeficienteDeDiagonalPrincipal = coeficientes[fila][fila];
+                double sumaDeFila = 0;
+
+                for (int columna = 0; columna < tamanioMatriz; columna++)
+                {
+                    if (fila != columna)
+                    {
+                        sumaDeFila += coeficientes[fila][columna]; 
+                    }
+                }
+
+                if (coeficienteDeDiagonalPrincipal < sumaDeFila)
+                {
+                    return DiagonalidadMatriz.NoDominante;
+                }
+                else
+                {
+                    if (coeficienteDeDiagonalPrincipal == sumaDeFila)
+                    {
+                        esEstrictamenteDominante = false;
+                    }
+                }
+
+            }
+
+            if (esEstrictamenteDominante)
+            {
+                return DiagonalidadMatriz.EstrictamenteDominante;
+            }
+
+            return DiagonalidadMatriz.Dominante;
         }
     }
 }
