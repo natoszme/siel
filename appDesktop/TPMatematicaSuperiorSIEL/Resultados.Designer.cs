8409270b7a545afca20ebbf129295fa4fc047b09
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-11-04 19:27:51 -0300

    Agrego un nuevo form para mostrar los resultados

---
+++ appDesktop/TPMatematicaSuperiorSIEL/Resultados.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TPMatematicaSuperiorSIEL
+{
+    partial class Resultados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(22, 46);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.Size = new System.Drawing.Size(240, 150);
+            this.dgvResultados.TabIndex = 0;
+            // 
+            // Resultados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dgvResultados);
+            this.Name = "Resultados";
+            this.Text = "Resultados";
+            this.Load += new System.EventHandler(this.Resultados_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvResultados;
+    }
+}
---
+++ appDesktop/TPMatematicaSuperiorSIEL/Resultados.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPMatematicaSuperiorSIEL
+{
+    public partial class Resultados : Form
+    {
+
+        public List<List<Double>> resultados = new List<List<double>>();
+
+        public Resultados(List<List<Double>> resultados)
+        {
+            this.resultados = resultados;
+            InitializeComponent();
+        }
+
+        private void Resultados_Load(object sender, EventArgs e)
+        {
+            mostrarResultadosEnTabla();
+        }
+
+        void mostrarResultadosEnTabla()
+        {
+            dgvResultados.ColumnCount = resultados[0].Count;
+            for (int r = 0; r < resultados.Count; r++)
+            {
+                dgvResultados.Columns[r].HeaderText = "X" + r;
+            }
+            for (int r = 0; r < resultados.Count; r++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dgvResultados);
+
+                for (int c = 0; c < resultados[0].Count; c++)
+                {
+                    row.Cells[c].Value = resultados[r][c];
+                }
+
+                dgvResultados.Rows.Add(row);
+            }
+        }
+    }
+}
