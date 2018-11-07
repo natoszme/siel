00e9bda521c4de9fa35b68f5b8f8e10af8c2a00e
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-11-07 20:26:53 -0300

    Agrego la clase PasoDeResolucion que contendra los valores de las incognitas y el resultado de los criterios de paro

---
+++ appDesktop/TPMatematicaSuperiorSIEL/PasoDeResolucion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMatematicaSuperiorSIEL
+{
+    public class PasoDeResolucion
+    {
+        public List<Double> valoresDeIncognitas;
+        public List<String> cumpleCriterioDeParo;
+
+        public PasoDeResolucion()
+        {
+            valoresDeIncognitas = new List<double>();
+            cumpleCriterioDeParo = new List<string>();
+        }
+
+        public void cargarCriterio(Boolean resultadoDelCriterio)
+        {
+            if (resultadoDelCriterio)
+            {
+                cumpleCriterioDeParo.Add("Sigo");
+            }
+            else
+            {
+                cumpleCriterioDeParo.Add("Paro");
+            }
+        }
+        public void setearValoresDeIncognitas(List<Double> valores)
+        {
+            valores.ForEach(agregarValor);
+        }
+
+        void agregarValor(Double valor){
+            valoresDeIncognitas.Add(valor);
+        }
+
+    }
+}
